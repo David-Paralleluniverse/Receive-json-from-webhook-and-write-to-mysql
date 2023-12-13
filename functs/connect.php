24f48ffdec8ca750577999c11f3918f97a05fc8e
David-Paralleluniverse <wy3h@outlook.com>
2023-12-13 20:27:32 +0800

    UPDATE software Config file-20231213

--- functs/connect.php
+++ functs/connect.php
@@ -1,6 +1,6 @@
 <?php
 	
-	$conn=mysqli_connect('localhost','root','123456','test');//可在此更改数据库连接信息('数据库服务器地址'，'用户名','密码','目标数据库')
+	$conn=mysqli_connect('localhost','root','root','test');//可在此更改数据库连接信息('数据库服务器地址'，'用户名','密码','目标数据库')
        //链接测试区，需要时可以解除注释
 /*
 	if($conn){
